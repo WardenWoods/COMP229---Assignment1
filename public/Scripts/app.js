72c176b54666e885f945451f15b25a3b75272272
Matthew Bradley <mjbradley321@gmail.com>
2021-10-02 02:04:23 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------------
+# app.js
+# Matthew Bradley
+# 301089966
+# 30 Sept 2021
+--------------------------------------------------------------*/
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
